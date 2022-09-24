4710187f2992425a3c129eca2c319cb4a9ef4b16
Jarod Shelton <jarodshelton01@gmail.com>
2022-09-24 08:25:43 -0500

    Implemented score manager

---
+++ Assets/Scripts/Utility/Score-Management/ScoreManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance { get; private set; }
+
+    public static int score = 0;
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+    }
+
+    public static void IncreaseScore(int n)
+    {
+        score += n;
+    }
+}
