37d9322eb96ed6cfd4e708856888141085266599
Mikey Bess <mikeybess27@gmail.com>
2022-09-24 12:51:46 -0700

    RAT MODE

--- Assets/Scripts/Enemy/RatParticles.cs
+++ Assets/Scripts/Enemy/RatParticles.cs
@@ -17,19 +17,9 @@ public class RatParticles : MonoBehaviour
         GetComponent<ParticleSystem>().trigger.SetCollider(0, _player.gameObject.transform);
     }
 
-    
-
     private void OnParticleTrigger()
     {
         int triggeredParticles = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, _particles);
-        int outsideParticles = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Outside, _particles);
-
-        for (int i = 0; i < outsideParticles; i++)
-        {
-            ParticleSystem.Particle p = _particles[i];
-            //p.;
-            _particles[i] = p;
-        }
 
         for (int i = 0; i < triggeredParticles; i++)
         {
