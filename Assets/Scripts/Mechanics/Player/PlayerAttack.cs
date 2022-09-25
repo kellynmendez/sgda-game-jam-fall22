31103bf5ec81d29515fe0cd89e0b09dadcf2aab8
Jarod Shelton <jarodshelton01@gmail.com>
2022-09-25 06:29:28 -0500

    Camera shake

--- Assets/Scripts/Mechanics/Player/PlayerAttack.cs
+++ Assets/Scripts/Mechanics/Player/PlayerAttack.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-
+    CameraMovement cam;
     public GameObject meleeAttack;
     public float range = 1;
     public float attackDuration = 0.1f;
     public float cooldownDuration = 0.1f;
     bool canAttack = true;
 
+    public float shakeAmp = 0.1f;
+    public float shakeDur = 0.1f;
+
     PlayerController playerMov;
 
     // Start is called before the first frame update
     void Start()
     {
+        cam = FindObjectOfType<CameraMovement>();
         playerMov = gameObject.GetComponent<PlayerController>();
     }
 
@@ -28,8 +32,9 @@ public class PlayerAttack : MonoBehaviour
             StartCoroutine(CooldownTimer(cooldownDuration));
             Vector3 spawnPoint = playerMov.velocity.normalized * range;
             float angle = Vector3.Angle(Vector3.right, spawnPoint);
+            cam.Shake(spawnPoint, shakeAmp, shakeDur);
             spawnPoint = transform.position + spawnPoint;
-            
+
             GameObject attackInstance = Instantiate(meleeAttack, spawnPoint, Quaternion.Euler(new Vector3(0, 0, angle)));
             attackInstance.transform.SetParent(transform);
             Destroy(attackInstance, attackDuration);
--- Assets/Scripts/Utility/CameraMovement.cs
+++ Assets/Scripts/Utility/CameraMovement.cs
@@ -7,6 +7,11 @@ public class CameraMovement : MonoBehaviour
     public float factor = -0.035f;
 
     PlayerController player = null;
+    Vector3 shakeVector = Vector3.zero;
+    float shakeDuration = 0f;
+    float shakeTimer = 0f;
+    float undirectedShakeAmplitude = 0f;
+    bool directedShake = true;
 
 
     private void Awake()
@@ -17,6 +22,46 @@ public class CameraMovement : MonoBehaviour
     private void LateUpdate()
     {
         // Apply the offset every frame to reposition this object
-        this.transform.position = player.transform.position + (player.velocity * factor);
+        Vector3 tempPosition = player.transform.position + (player.velocity * factor);
+
+        if (shakeTimer < shakeDuration)
+        {
+            if (directedShake)
+            {
+                tempPosition += shakeVector * Mathf.Sin(Mathf.PI / shakeDuration * shakeTimer);
+            }
+            else
+            {
+                float angle = Random.Range(0f, Mathf.PI * 2f);
+                Vector3 randomVector = new Vector3(Mathf.Cos(angle) * undirectedShakeAmplitude, Mathf.Sin(angle) * undirectedShakeAmplitude, 0);
+                tempPosition += randomVector * Mathf.Sin(Mathf.PI / shakeDuration * shakeTimer);
+            }
+
+            shakeTimer += Time.deltaTime;
+        }
+
+        transform.position = tempPosition;
     }
+    public void UndirectedShake(float amplitude, float duration)
+    {
+        if (shakeTimer >= shakeDuration || amplitude >= shakeVector.magnitude)
+        {
+            directedShake = false;
+            shakeDuration = duration;
+            undirectedShakeAmplitude = amplitude;
+        }
+    }
+
+    public void Shake(Vector3 direction, float amplitude, float duration)
+    {
+        //Debug.Log(shakeTimer+"/"+shakeDuration+" | "+amplitude+"/"+shakeVector.magnitude);
+        if (shakeTimer >= shakeDuration || amplitude >= shakeVector.magnitude)
+        {
+            directedShake = true;
+            shakeVector = direction.normalized * amplitude;
+            shakeDuration = duration;
+            shakeTimer = 0f;
+        }
+    }
+
 }
