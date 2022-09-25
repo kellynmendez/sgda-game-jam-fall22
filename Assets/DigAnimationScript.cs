44110e99e6becb40d87c071427b1f6fc2498cfdf
Mikey Bess <mikeybess27@gmail.com>
2022-09-25 05:03:35 -0700

    animation

---
+++ Assets/DigAnimationScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigAnimationScript : MonoBehaviour
+{
+
+
+
+    public void DigEvent(int s)
+    {
+        //Camera Shake
+        //Spawn Trash
+    }
+}
