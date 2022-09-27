32e8d11acb377f95ac5fb5ef11700908e23cf908
Kellyn Mendez <kpm.mendez@gmail.com>
2022-09-27 09:42:18 -0500

    fixed credits for real

--- Assets/Scripts/UI/CreditsUI.cs
+++ Assets/Scripts/UI/CreditsUI.cs
@@ -5,20 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class CreditsUI : MonoBehaviour
 {
-    [SerializeField] float _duration = 10f;
-    RectTransform _transform;
-    Vector3 from;
-    Vector3 to;
-    bool creditsDone = false;
-
-    private void Awake()
-    {
-        _transform = GetComponent<RectTransform>();
-        from = _transform.position;
-        to = new Vector3(_transform.position.x, _transform.position.y + 3000, _transform.position.z);
-        StartCoroutine(LerpPosition(_transform, from, to, _duration));
-    }
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -29,29 +15,5 @@ public class CreditsUI : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
-        if (creditsDone)
-        {
-            SceneManager.LoadScene(0);
-        }
-    }
-
-    public IEnumerator LerpPosition(Transform target, Vector3 from, Vector3 to, float duration)
-    {
-        // initial value
-        target.position = from;
-
-        // animate value
-        float elapsedTime = 0;
-        while (elapsedTime < duration)
-        {
-            target.position = Vector3.Lerp(from, to, elapsedTime / duration);
-
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        // final value
-        target.position = to;
-        creditsDone = true;
     }
 }
